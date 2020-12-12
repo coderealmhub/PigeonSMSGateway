fbc60941b98a4df44f5589315f7eb52c21ad018d
netoeac <eliasneto1404@gmail.com>
2020-12-12 00:35:24 -0300

    added splashScreen and icon

---
+++ app/src/main/java/br/com/coderealm/realmsms/SplashActivity.java
@@ -0,0 +1,32 @@
+package br.com.coderealm.realmsms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showMainActivity();
+            }
+        }, 3000);
+    }
+
+    private void showMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
